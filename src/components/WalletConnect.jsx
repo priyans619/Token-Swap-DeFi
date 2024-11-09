a8ec80eb92f7775082605b4bf0d5eb25514fd7e0
priyans619 <priyansh.2b@gmail.com>
2024-11-09 21:43:14 +0530

    wallet connect component

---
+++ src/components/WalletConnect.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const WalletConnect = () => {
+
+  return (
+    <div className="">
+      <button
+        onClick={}
+        className=""
+      >
+        Connect Wallet
+      </button>
+    </div>
+  );
+};
+
+export default WalletConnect;
