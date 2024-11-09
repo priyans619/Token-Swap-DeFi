24d159bfa3c6ef2e95a70dbe0ad0f9a42f45b971
priyans619 <priyansh.2b@gmail.com>
2024-11-09 21:46:37 +0530

    button interface

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -1,13 +1,12 @@
-
 import React from 'react';
 
 const WalletConnect = () => {
 
   return (
-    <div className="">
+    <div className="bg-gray-900 p-4 rounded-lg text-white">
       <button
         onClick={}
-        className=""
+        className="bg-violet-500 text-white font-bold py-2 px-4 rounded"
       >
         Connect Wallet
       </button>
