c836787bc856ac1bed232dec4ffce5cad7e72e82
priyans619 <priyansh.2b@gmail.com>
2024-11-27 17:32:19 +0530

    nav section background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,11 @@ import TokenSwap from '../components/TokenSwap';
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-900 p-4">
-      <div className="flex items-center justify-between mb-6">
-        <h1 className="text-3xl font-bold text-violet-400">Token Swap</h1>
+    <div className="min-h-screen bg-gradient-to-b from-black to-blue-200 via-black p-4 bg-[length:100%_100%]">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-yellow-300 via-white to-violet-400">
+          DeFi Exchange
+        </h1>
       </div>
       <WalletConnect />
       <TokenSwap />
