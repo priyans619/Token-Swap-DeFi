3e137a0ffcb6bb0e61d04324056287302f499dc1
priyans619 <priyansh.2b@gmail.com>
2024-11-27 17:47:21 +0530

    update wallet

--- src/components/WalletConnect.jsx
+++ src/components/WalletConnect.jsx
@@ -5,19 +5,20 @@ const WalletConnect = () => {
   const { address, balance, connectWallet } = useWallet();
 
   return (
-    <div className="bg-gray-900 p-4 rounded-lg text-white">
-      <div className="absolute top-4 right-4">
+    <div className="bg-black rounded-lg text-white">
+      <div className="absolute top-3 right-4">
         <button
           onClick={connectWallet}
-          className="bg-violet-500 text-white font-bold py-2 px-4 rounded"
+          className="bg-gradient-to-r from-yellow-500 via-violet-500 to-blue-500 text-white font-bold py-2 px-4 rounded-3xl border-2 border-blue-500 transition duration-300 hover:bg-black hover:from-black hover:via-black hover:to-black"
         >
-          Connect Wallet
+          Goemon Wallet
         </button>
+
       </div>
 
       {/* Wallet Information */}
       {address && (
-        <div className="mt-4 flex flex-col items-center justify-centerp-6 rounded-lg shadow-lg">
+        <div className=" flex flex-col items-center justify-center mb-6 p-1 mt-2 rounded-lg shadow-lg">
           <p className="text-lg mb-2"><strong>Wallet Address:</strong> {address}</p>
           <p className="text-lg"><strong>Balance:</strong> {balance} ETH</p>
         </div>
