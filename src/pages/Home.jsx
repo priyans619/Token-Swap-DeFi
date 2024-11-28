b6b7257a4214e6dc843b474315f3017a965234f2
priyans619 <priyansh.2b@gmail.com>
2024-11-28 16:35:04 +0530

    add titles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,13 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-black to-blue-200 via-black p-4">
       <Navbar />
-      <div className="flex flex-col lg:flex-row mt-6">
+      <div className="flex flex-col lg:flex-row mt-10">
         <div className="flex-1 mr-8">
+          <h2 className="text-2xl font-semibold text-white mb-4 ml-6">Token Swap</h2>
           <TokenSwap />
         </div>
         <div className="flex-1 mr-10">
+          <h2 className="text-2xl font-semibold text-white mb-4">Order Book</h2>
           <OrderBook />
         </div>
       </div>
