ade75e233a6b5df96d3de509c07fadd9230fc345
priyans619 <priyansh.2b@gmail.com>
2024-11-28 20:24:14 +0530

    add toggle functionalty with react-icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import { useWallet } from '../hooks/useWallet';
 
-const Navbar = () => {
+const Navbar = ({ isDarkMode, toggleDarkMode }) => {
   const { connectWallet } = useWallet();
 
+  const handleDarkModeToggle = () => {
+    toggleDarkMode(!isDarkMode); // Pass the updated mode to the parent
+  };
+
   return (
-    <div className="bg-black text-white p-3 flex justify-between items-center border-b-2 border-gray-600">
-      <h1 className="text-3xl ml-3 font-bold text-transparent bg-clip-text bg-gradient-to-b from-yellow-300 via-white to-violet-400">
+    <div
+      className={`p-3 flex justify-between items-center border-b-2 ${
+        isDarkMode ? 'bg-black text-white border-gray-600' : 'bg-white text-black border-gray-300'
+      }`}
+    >
+      <h1 className="text-3xl ml-3 font-bold text-transparent bg-clip-text bg-gradient-to-b from-yellow-300 via-gray-300 to-violet-400">
         DeFi Exchange
       </h1>
-      <button
-        onClick={connectWallet}
-        className="bg-gradient-to-r from-yellow-500 via-violet-500 to-blue-500 mr-5 text-white font-bold py-2 px-7 rounded-2xl border-2 border-blue-500 transition duration-300 hover:bg-black hover:from-black hover:via-black hover:to-black"
-      >
-        Goemon Wallet
-      </button>
+      <div className="flex items-center">
+        <button
+          onClick={connectWallet}
+          className="bg-gradient-to-r from-yellow-500 via-violet-500 to-blue-500 mr-3 text-white font-bold py-2 px-7 rounded-2xl border-2 border-blue-500 transition duration-300 hover:bg-black hover:from-black hover:via-black hover:to-black"
+        >
+          Goemon Wallet
+        </button>
+        <button
+          onClick={handleDarkModeToggle}
+          className={`text-2xl ${isDarkMode ? 'text-white' : 'text-black'}`}
+        >
+          {isDarkMode ? <MdDarkMode /> : <MdLightMode />}
+        </button>
+      </div>
     </div>
   );
 };
